c89b2416a69d7398d9cb5c0879381bae74974fe6
Pratik JH <pratik.jh2017@gmail.com>
2022-07-03 14:47:05 +0530

    Database initialized and populated dummy data

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log("HELLO WORLD!");
+const { faker } = require('@faker-js/faker');
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "join_us"
+});
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
+
+
+// var q = "SELECT COUNT(*) AS total FROM users";
+
+// con.query(q, function(error, results, fields){
+//     if (error) throw error;
+//     console.log(results[0].total);
+// });
+
+
+var persons = [];
+
+
+for (var i = 0; i < 500; i++){
+    var newperson = [faker.internet.email(), faker.date.past()];
+
+    persons.push(newperson);
+}
+
+var q = "INSERT INTO users (email, created_at) VALUES ?"
+
+con.query(q, [persons], function(err, result){
+    if(err) throw err;
+    console.log(result);
+});
+
+
+con.end();
---
+++ exercises.sql
@@ -0,0 +1,44 @@
+-- 1.
+
+SELECT 
+    DATE_FORMAT(MIN(created_at), '%M %D %Y') AS earliest_date 
+FROM users;
+
+
+-- 2.
+
+SELECT 
+    email, created_at
+FROM users
+WHERE created_at = (SELECT MIN(created_at) FROM users);
+
+-- 3.
+
+SELECT 
+    MONTHNAME(created_at) AS month, 
+    COUNT(*) AS count
+FROM users
+GROUP BY month
+ORDER BY count DESC;
+
+-- 4.
+
+SELECT 
+    COUNT(*) AS yahoo_users
+FROM users
+WHERE email LIKE "%@yahoo.com";
+
+
+-- 5.
+
+SELECT
+    CASE
+        WHEN email LIKE "%yahoo.com" THEN "yahoo"
+        WHEN email LIKE "%gmail.com" THEN "gmail"
+        WHEN email LIKE "%hotmail.com" THEN "hotmail"
+        ELSE "other"
+    END AS provider,
+    COUNT(*) AS total_users
+FROM users
+GROUP BY provider
+ORDER BY total_users DESC;
